dec6af79360ae0c7f7284e6b474d6b327563f06a
Merphyx <merphyx@gmail.com>
2024-02-24 23:43:27 +0100

    Now for Fabric 1.20.4!

---
+++ src/main/java/me/lunaluna/find/fabric/mixins/HandledScreenMixin.java
@@ -0,0 +1,77 @@
+package me.lunaluna.find.fabric.mixins;
+
+import me.lunaluna.find.fabric.Startup;
+import me.lunaluna.find.fabric.config.Config;
+import me.lunaluna.find.fabric.widget.FindWidget;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.text.Text;
+import com.mojang.blaze3d.systems.RenderSystem;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HandledScreen.class)
+public abstract class HandledScreenMixin extends Screen {
+    @Shadow protected int x;
+    @Shadow protected int y;
+    @Shadow protected int backgroundHeight;
+
+    protected HandledScreenMixin(Text title) { super(title); }
+    private static final boolean HAS_BORDER = true;
+    private FindWidget widget;
+
+    @Inject(at = @At("TAIL"), method = "init")
+    private void init(CallbackInfo ci) {
+        int yOffset = 4;
+        widget = new FindWidget(x + 1, y + backgroundHeight + yOffset);
+        addSelectableChild(widget);
+    }
+
+    @Inject(at = @At("HEAD"), method = "keyPressed", cancellable = true)
+    private void disableFurtherProcessing(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+        if (widget.isFocused()) {
+            if (keyCode == GLFW.GLFW_KEY_ESCAPE || keyCode == GLFW.GLFW_KEY_ENTER) {
+                focusOn(null);
+                widget.setFocused(false);
+            }
+            widget.keyPressed(keyCode, scanCode, modifiers);
+            cir.setReturnValue(true);
+        } else if (Startup.toggle.matchesKey(keyCode, scanCode) && modifiers == GLFW.GLFW_MOD_CONTROL) {
+            focusOn(widget);
+            widget.setFocused(true);
+            widget.setSelectionStart(0);
+            widget.setSelectionEnd(widget.getText().length());
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(at = @At("TAIL"), method = "drawSlot")
+    private void darkenNonMatching(DrawContext context, Slot slot, CallbackInfo ci) {
+        if (!FindWidget.search.isBlank() && !widget.matches(slot.getStack())) {
+            darkenSlot(context, slot.x, slot.y);
+        }
+    }
+
+    @Inject(at = @At("TAIL"), method = "render")
+    private void renderSearch(DrawContext matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        widget.renderFindWidget(matrices, mouseX, mouseY, delta);
+    }
+
+    public void darkenSlot(DrawContext matrices, int x, int y) {
+        var color = Config.INSTANCE.color().getRGB();
+        var border = HAS_BORDER ? 1 : 0;
+
+        RenderSystem.disableDepthTest();
+        RenderSystem.colorMask(true, true, true, false);
+        matrices.fillGradient(x - border, y - border, x + 16 + border, y + 16 + border, 100, color, color);
+        RenderSystem.colorMask(true, true, true, true);
+        RenderSystem.enableDepthTest();
+    }
+}
---
+++ src/main/java/me/lunaluna/find/fabric/mixins/ItemRendererMixin.java
@@ -0,0 +1,36 @@
+package me.lunaluna.find.fabric.mixins;
+
+import me.lunaluna.find.fabric.widget.FindWidget;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+
+    @Inject(at = @At("HEAD"), method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V")
+    public void beforeRenderItem(ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
+        if (!FindWidget.search.isBlank()) {
+            FindWidget findWidget = FindWidget.getInstance();
+            boolean matchesSearch = !stack.isEmpty() && findWidget.matches(stack);
+            matrices.push();
+            float scale = matchesSearch ? 1.2f : 0.8f;
+            matrices.scale(scale, scale, scale);
+        }
+    }
+
+    @Inject(at = @At("RETURN"), method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V")
+    public void afterRenderItem(ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
+        if (!FindWidget.search.isBlank()) {
+            matrices.pop();
+        }
+    }
+}
